359c77b953f7942775b3c7057cf5c764de690bd3
Jingtao Li <36227808+Jingtao-Li-CVer@users.noreply.github.com>
2023-04-21 12:08:54 +0800

    Update base_runner.py

--- runners/base_runner.py
+++ runners/base_runner.py
@@ -19,20 +19,6 @@ import torch.distributed as dist
 from torch.nn.parallel import DistributedDataParallel as DDP
 
 
-def weights_init(mod):
-    """
-    Custom weights initialization called on netG, netD and netE
-    :param m:
-    :return:
-    """
-    classname = mod.__class__.__name__
-    if classname.find('Conv') != -1:
-        mod.weight.data.normal_(0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
-        mod.weight.data.normal_(1.0, 0.02)
-        mod.bias.data.fill_(0)
-
-
 class BaseRunner(ABC):
     def __init__(self, cfg: DictConfig, working_dir: str) -> None:
         super().__init__()
